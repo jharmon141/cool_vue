e1f60864790309314d4e601476725b16951c8196
jharmon141 <jeremy.l.harmon@gmail.com>
2017-06-18 00:39:07 -0500

    :alien:

--- template/src/App.vue
+++ template/src/App.vue
@@ -14,6 +14,24 @@ export default {
 </script>
 
 <style>
+
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+body {
+    margin: 0 0 100px; /* bottom = footer height */
+}
+
+footer {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 100px;
+    width: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
