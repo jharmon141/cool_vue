29ae658f1d8cc4e58871637ea15c05ecf82570bd
jharmon141 <jeremy.l.harmon@gmail.com>
2017-06-27 20:38:38 -0500

    :robot:

--- template/dist/server.js
+++ template/dist/server.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
-var port = process.env.PORT || 5000;
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const port = process.env.PORT || 5000;
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.load();
 
 app = express();
 app.use(history());
 app.use(serveStatic(__dirname));
+
+
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
+app.use(bodyParser.json());
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.listen(port);
 console.log('server started '+ port);
--- template/server.js
+++ template/server.js
@@ -1,8 +1,30 @@
-var express = require(‘express’);
-var path = require(‘path’);
-var serveStatic = require(‘serve-static’);
+const express = require(‘express’);
+const path = require(‘path’);
+const bodyParser = require('body-parser');
+const history = require('connect-history-api-fallback');
+const serveStatic = require(‘serve-static’);
+const port = process.env.PORT || 5000;
+
+app.use(history());
+
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
+app.use(bodyParser.json());
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app = express();
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
+
+
+
+
+
+
 app.listen(port);
 console.log(‘server started ‘+port);
